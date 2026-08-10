17ba71b5440cea634cd66314a150aed6a0ae9831
Beast0E4 <ebison.04@gmail.com>
2026-08-10 19:25:44 +0530

    chore: bootstrap project with build config and application properties

---
+++ src/main/java/com/enginecorp/workfloworchestrator/WorkflowOrchestratorApplication.java
@@ -0,0 +1,16 @@
+package com.enginecorp.workfloworchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableAsync
+@EnableScheduling
+public class WorkflowOrchestratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkflowOrchestratorApplication.class, args);
+    }
+}
